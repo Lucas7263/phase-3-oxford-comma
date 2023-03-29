7777ca6974ebace7db8ed130a6688db1708cc167
Lucas <lucaswoodyard@yahoo.com>
2023-03-29 18:57:22 -0500

    commit tree ea4ddd407ecf4e23de1d81d219fb8ff4c9c8c0b5

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+   if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+   elsif 2 < array.length
+        array[-1].insert(0, "and ")
+   end
+        array.join(", ")
 
-end
+    # return array.join("and ") if array.size < 3
+
+       
+
+        # array.join(", ")
+    end
